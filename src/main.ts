4c6f49e4a0b2cfaa7ce0de2535f3bcb250a9a21c
Andre Hopf <andre.hopf@gmail.com>
2024-09-15 15:54:14 -0300

    feat: add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
-console.log('hello world')
-console.log('teste')
+const loginForm = document.getElementById('login-form')!;
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    console.log(e.target)
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,75 @@
 const path = require('path')
-const Copylugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
     mode: 'production',
     entry: {
-        index: './src/main.ts', // Existing entry point
-        about: './src/about.ts', // New entry point for about.html
+        index: './src/main.ts',
+        about: './src/about.ts',
+    },
+    output: {
+        filename: '[name].min.js', // Output filenames based on entry names
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            chunks: ['index'],
+            filename: 'index.html',
+            minify: false
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/about.html',
+            chunks: ['about'],
+            filename: 'about.html',
+            minify: false
+        }),
+    ],
+    resolve: {
+        extensions: ['.ts', '.js']
     },
     devServer: {
         static: path.join(__dirname, 'dist'),
         port: 9000,
         hot: true
     },
-    output: {
-        filename: '[name].min.js', // Output filenames based on entry names
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-        // filename: "app.min.js",
-        // path: path.join(__dirname, 'dist')
-    },
-    plugins: [
-        new Copylugin({
-            patterns: [{from: 'public'}]
-        })
-    ],
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
     module: {
-        rules: [{
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-        }]
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
     }
 }
